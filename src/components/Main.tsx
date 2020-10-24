de28b48eb967731f0ba6fc6ded4570cc725fb6d2
Jonathon Alder <jonathon.alder@gmail.com>
2020-10-24 17:35:13 -0400

    remove onscroll behavior

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,31 +1,14 @@
 import React, { ReactElement, useEffect, useState } from 'react';
-
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Post } from '../types';
 import { Nav } from './Nav';
 import { Blog } from './Blog';
 import { getAllPosts } from '../util/aws';
+import { NotFound } from './NotFound';
 
 export function Main(): ReactElement {
-  const [posts, setPosts] = useState<Array<Post>>();
-  const [currentPost, setCurrentPost] = useState<Post>();
+  const [posts, setPosts] = useState<Post[]>();
   const [loading, setLoading] = useState<boolean>(false);
-
-  let prevScrollPos = window.pageYOffset;
-  window.onscroll = function () {
-    console.log('scrolling');
-    const currScrollPos = window.pageYOffset;
-    console.log(currScrollPos);
-    const nav = document.getElementById('nav');
-    if (nav) {
-      if (prevScrollPos > currScrollPos) {
-        nav.style.top = '0';
-      } else {
-        nav.style.top = '-50px';
-      }
-      prevScrollPos = currScrollPos;
-    }
-  };
 
   /* load posts on mount */
   useEffect(() => {
@@ -35,14 +18,11 @@ export function Main(): ReactElement {
         const getPosts = await getAllPosts();
         if (getPosts) {
           setPosts(getPosts);
-          setCurrentPost(getPosts[0]);
         } else {
           setPosts([]);
         }
         setLoading(false);
       })();
-    } else if (posts && posts.length > 0) {
-      setCurrentPost(posts[0]);
     }
   }, [posts, loading]);
 
@@ -52,12 +32,13 @@ export function Main(): ReactElement {
         <Nav posts={posts} />
         <Switch>
           <Route path={['/', '/blog', '/blog/post/:title']}>
-            <Blog posts={posts} currentPost={currentPost} />
+            <Blog posts={posts} />
+          </Route>
+          <Route path="*">
+            <NotFound />
           </Route>
         </Switch>
       </Router>
     </div>
   );
 }
-
-Main.displayName = 'Main';
