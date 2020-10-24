b0b403ac3951f70ad8ab7016f2f97e82b7961e71
Jonathon Alder <jonathon.alder@gmail.com>
2020-10-24 17:35:29 -0400

    404 wip-

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+
+export function NotFound(): ReactElement {
+  return (
+    <div id="not-found">
+      <span>Page not found.</span>
+    </div>
+  );
+}
