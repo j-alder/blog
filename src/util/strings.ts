f290cc56923638306d95a440eca2161f0ebbb457
Jonathon Alder <jonathon.alder@gmail.com>
2020-11-08 18:55:29 -0500

    strings helpers

--- src/util/strings.ts
+++ src/util/strings.ts
@@ -1,3 +1,24 @@
+/** Convert space delimited string to lowercase kebab-case */
 export function fmtTitle(str: string): string {
   return str.split(' ').join('-').toLowerCase();
+}
+
+/** Format an ISO date string into a readable date */
+export function fmtDate(isoStr?: string): string | undefined {
+  if (!isoStr) {
+    return;
+  }
+  return new Date(isoStr).toLocaleDateString('en-US');
+}
+
+export function fmtArchiveLinkText(
+  title: string,
+  date: string,
+): string | undefined {
+  const formattedDate = fmtDate(date);
+  if (formattedDate) {
+    const len = 90 - title.length;
+    return title + '.'.repeat(len) + formattedDate;
+  }
+  return;
 }
