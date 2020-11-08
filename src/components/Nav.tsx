752d624d05b725a48d327f18a093e3ef5a80e7e6
Jonathon Alder <jonathon.alder@gmail.com>
2020-11-08 18:54:11 -0500

    responsive nav styles

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Post } from '../types';
+import { fmtArchiveLinkText } from '../util/strings';
 import { Blurb } from './Blurb';
 
 interface Props {
@@ -10,6 +11,8 @@ interface Props {
 export function Nav({ posts }: Props): ReactElement {
   const [expanded, setExpanded] = useState<boolean>(false);
   const [activeSection, setActiveSection] = useState<string>();
+
+  const desktop = window.innerWidth > 999;
 
   const history = useHistory();
 
@@ -34,56 +37,83 @@ export function Nav({ posts }: Props): ReactElement {
   }
 
   return (
-    <div id="nav" className={`flex-column ${expanded ? 'expanded' : ''}`}>
-      <div className="nav__header">
-        <div className="flex-row nav__expand-collapse">
-          <button onClick={() => setExpanded((exp) => !exp)}>
-            <i className={`fas fa-plus ${expanded ? 'expanded' : ''}`}></i>
-          </button>
-        </div>
-        <Blurb />
-      </div>
-      <div className="flex-column">
-        {/* about */}
-        <div className={`flex-column nav__drawer-header ${isActive('about')}`}>
-          <button onClick={() => setActive('about')}>ABOUT</button>
-          <hr />
-        </div>
-        <div className={`nav__section ${isActive('about')}`}>
-          <span>
-            I&apos;m a software developer navigating the tech industry in
-            Richmond, Virginia. This blog is meant to be as wandering as my
-            interests, which typically revolve around bicycling, camping,
-            programming and preparing to be a dad.
-          </span>
+    <div
+      id="nav"
+      className={`flex-column${expanded ? ' expanded' : ''}${
+        desktop ? ' desktop' : ''
+      }`}
+    >
+      <div>
+        <div
+          className={`nav__header${desktop && !expanded ? ' desktop-min' : ''}`}
+        >
+          <div
+            className={`flex-row nav__expand-collapse${
+              desktop && !expanded ? ' desktop-min' : ''
+            }`}
+          >
+            <button onClick={() => setExpanded((exp) => !exp)}>
+              <i className={`fas fa-plus ${expanded ? 'expanded' : ''}`}></i>
+            </button>
+          </div>
+          <Blurb desktop={desktop} navExpanded={expanded} />
         </div>
 
-        {/* archive */}
-        <div
-          className={`flex-column nav__drawer-header ${isActive('archive')}`}
-        >
-          <button onClick={() => setActive('archive')}>ARCHIVE</button>
-          <hr />
-        </div>
-        <div className={`nav__section ${isActive('archive')}`}>
-          <ul id="posts">
-            {posts ? (
-              posts.map((post) => (
-                <li key={post.created} className="nav__archive-link">
-                  <button onClick={() => navToPost(post.title)}>
-                    {post.title}
-                  </button>
-                </li>
-              ))
-            ) : (
-              <span>No posts in archive.</span>
-            )}
-          </ul>
-        </div>
+        {expanded && (
+          <div className="flex-column">
+            {/* about */}
+            <div
+              className={`flex-column nav__drawer-header ${isActive('about')}`}
+            >
+              <button onClick={() => setActive('about')}>ABOUT</button>
+              <hr />
+            </div>
+            <div className={`nav__section ${isActive('about')}`}>
+              <span>
+                I&apos;m a software developer navigating the tech industry in
+                Richmond, Virginia. This blog is meant to be as wandering as my
+                interests, which typically revolve around bicycling, camping,
+                programming and preparing to be a dad.
+              </span>
+            </div>
+
+            {/* archive */}
+            <div
+              className={`flex-column nav__drawer-header ${isActive(
+                'archive',
+              )}`}
+            >
+              <button onClick={() => setActive('archive')}>ARCHIVE</button>
+              <hr />
+            </div>
+            <div className={`nav__section ${isActive('archive')}`}>
+              <ul id="posts">
+                {posts ? (
+                  posts.map((post) => (
+                    <li key={post.created} className="nav__archive-link">
+                      <button onClick={() => navToPost(post.title)}>
+                        {fmtArchiveLinkText(
+                          post.title,
+                          post.updated || post.created,
+                        )}
+                      </button>
+                    </li>
+                  ))
+                ) : (
+                  <span>No posts in archive.</span>
+                )}
+              </ul>
+            </div>
+          </div>
+        )}
       </div>
 
       {/* social media */}
-      <div className="nav__media-links">
+      <div
+        className={`nav__media-links${
+          desktop && !expanded ? ' desktop-min' : ''
+        }`}
+      >
         <a
           href="https://github.com/j-alder"
           target="_blank"
